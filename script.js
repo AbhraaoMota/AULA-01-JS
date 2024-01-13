fe23fb6eac872213c522362bfaf9c066f6585d3c
AbhraaoMota <149906527+AbhraaoMota@users.noreply.github.com>
2024-01-13 13:39:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+//Atividade 1
+// let nome = prompt("Digite seu nome:")
+// alert("Seu nome é: " + nome + " Seja Bem-Vindo")
+// console.log(nome)
+
+//Atividade 2
+// let num1 = Number(prompt("Digite um numero"))
+// let num2 = Number(prompt("Digite um numero"))
+// alert("O resultado da soma é: " + (num1 + num2) )
+//conlose.log(num1 + num2)
+
+// //Atividade 3
+// let idade = Number(prompt("Digite sua idade"))
+// let ano = 2024
+// alert("O ano que voce nasceu é: "+ (ano - idade))
+//console.log(ano - idade)
+
+//Atividade 4
+// let raio = Number(prompt("Digite o raio"))
+// let pi = 3.14
+
+// let result = pi * raio ** 2
+// alert("o raio do circulo é de: " + result)
+
+//Atividade 5
+// let dolar = Number(prompt("Digite o valor"))
+// let euro = 0.85
+
+// let soma = dolar * euro
+// alert(dolar + " de dolar custa " + soma + " de Euro")
+// console.log(soma)
+
+// Atividade 6
+// let preço = Number(prompt("Digite o valor"))
+// let promo = 10
+ 
+// let desc = preço / promo
+// let final = preço - desc 
+// alert("O valor do desconto é R$" + desc  +"\n"+
+//     "O valor com o desconto ficou em R$" + final)
+
+//Atividade 7
+// let m = Number(prompt("Digite quantos metros voce deseja"))
+// let cm = 100
+
+// let s = m * cm
+// alert( m + " metros fica em centimentros " + s)
+
+//desafio
+
+// let hm = Number(prompt("Horas trabalhadas no mês"))
+// let vp = 5.50
+// let im = 11
+// let inss = 8
+// let sin = 5
+
+// let salario = hm * vp
+// let s_im = salario / im
+// let im_inss = s_im / inss
+// let inss_sin = im_inss / sin
+// let receber = salario - s_im - im_inss - inss_sin
+
+
+
+// alert( "Seu salario sera R$" + salario + "\n" + "Voce pagara de imposto R$" + s_im + "\n" + "Voce pagara de INSS R$" + im_inss + "\n" + "Voce pagara de Sindicato R$" + inss_sin + "\n" + "Incluindo todos as taxas voce recebera R$" + receber)
+
